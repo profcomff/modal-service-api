47222f27e0fbacfe4e963a88a58cc01ac9cf204d
petrCher <88943157+petrCher@users.noreply.github.com>
2026-04-12 20:38:27 +0300

    models

--- modal_backend/models/db.py
+++ modal_backend/models/db.py
@@ -1,5 +1,17 @@
 from __future__ import annotations
 
+import datetime
+from enum import Enum
+
+from sqlalchemy import (
+    Boolean,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+)
+from sqlalchemy.orm import Mapped, mapped_column
+
 from modal_backend.settings import Settings, get_settings
 
 from .base import BaseDbModel
@@ -8,5 +20,61 @@ from .base import BaseDbModel
 settings: Settings = get_settings()
 
 
-class Class(BaseDbModel):
-    pass
+class ModalStatus(str, Enum):
+    ACTIVE: str = "active"
+    ARCHIEVED: str = "archieved"
+
+
+class NoteType(BaseDbModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str | None] = mapped_column(String, nullable=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+
+class Group(BaseDbModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    group_id: Mapped[int] = mapped_column(Integer)
+    name: Mapped[str | None] = mapped_column(String, nullable=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+
+class Service(BaseDbModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    service_id: Mapped[int] = mapped_column(Integer)
+    name: Mapped[str | None] = mapped_column(String, nullable=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+
+class Note(BaseDbModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    type_id: Mapped[int] = mapped_column(Integer, ForeignKey("NoteType.id"))
+    header: Mapped[str] = mapped_column(String, nullable=False)
+    info_text: Mapped[str | None] = mapped_column(String, nullable=True)  # type_id=1
+    rating_max: Mapped[int | None] = mapped_column(Integer, nullable=True)  # type_id=2
+    text: Mapped[str | None] = mapped_column(String, nullable=True)  # type_id=3
+    max_length: Mapped[int | None] = mapped_column(Integer, nullable=True)  # type_id=3
+    choice_options: Mapped[list[dict] | None] = mapped_column(String, nullable=True)  # type_id=4
+    is_multiple: Mapped[bool | None] = mapped_column(Boolean, nullable=True)  # type_id=4
+    images: Mapped[list[str] | None] = mapped_column(String, nullable=True)  # type_id=5
+    group_ids: Mapped[list[int]] = mapped_column(Integer)
+    service_ids: Mapped[list[int]] = mapped_column(Integer)
+    frequency: Mapped[int] = mapped_column(Integer)
+    start_ts: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
+    env_ts: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
+    updated_ts: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
+    is_always: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    admin_id: Mapped[int] = mapped_column(Integer)
+    status: Mapped[ModalStatus] = mapped_column(String, nullable=False)
+    view_count: Mapped[int] = mapped_column(Integer, default=0)
+    rejected_count: Mapped[int] = mapped_column(Integer, default=0)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+
+class NoteResponse(BaseDbModel):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    note_id: Mapped[int] = mapped_column(Integer, ForeignKey("Note.id"))
+    user_id: Mapped[int] = mapped_column(Integer)
+    rating: Mapped[int | None] = mapped_column(Integer, nullable=True)  # type_id=2
+    text: Mapped[str | None] = mapped_column(String, nullable=True)  # type_id=3
+    selected_choices: Mapped[list[dict] | None] = mapped_column(String, nullable=True)  # type_id=4
+    is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
--- modal_backend/routes/exc_handlers.py
+++ modal_backend/routes/exc_handlers.py
@@ -1,17 +1,11 @@
 import starlette.requests
 from starlette.responses import JSONResponse
 
-# check exceptions
 from modal_backend.exceptions import (
     AlreadyExists,
-    CommentTooLong,
     ForbiddenAction,
-    ForbiddenSymbol,
     ObjectNotFound,
-    TooManyCommentRequests,
-    TooManyCommentsToLecturer,
     UpdateError,
-    WrongMark,
 )
 from modal_backend.schemas.base import StatusResponseModel
 
@@ -32,45 +26,10 @@ async def already_exists_handler(req: starlette.requests.Request, exc: AlreadyEx
     )
 
 
-@app.exception_handler(TooManyCommentRequests)
-async def too_many_comment_handler(req: starlette.requests.Request, exc: AlreadyExists):
-    return JSONResponse(
-        content=StatusResponseModel(status="Error", message=exc.eng, ru=exc.ru).model_dump(), status_code=429
-    )
-
-
-@app.exception_handler(TooManyCommentsToLecturer)
-async def too_many_comment_handler(req: starlette.requests.Request, exc: AlreadyExists):
-    return JSONResponse(
-        content=StatusResponseModel(status="Error", message=exc.eng, ru=exc.ru).model_dump(), status_code=429
-    )
-
-
 @app.exception_handler(ForbiddenAction)
 async def forbidden_action_handler(req: starlette.requests.Request, exc: ForbiddenAction):
     return JSONResponse(
         content=StatusResponseModel(status="Error", message=exc.eng, ru=exc.ru).model_dump(), status_code=403
-    )
-
-
-@app.exception_handler(WrongMark)
-async def wrong_mark_handler(req: starlette.requests.Request, exc: WrongMark):
-    return JSONResponse(
-        content=StatusResponseModel(status="Error", message=exc.eng, ru=exc.ru).model_dump(), status_code=400
-    )
-
-
-@app.exception_handler(CommentTooLong)
-async def comment_too_long_handler(req: starlette.requests.Request, exc: CommentTooLong):
-    return JSONResponse(
-        content=StatusResponseModel(status="Error", message=exc.eng, ru=exc.ru).model_dump(), status_code=400
-    )
-
-
-@app.exception_handler(ForbiddenSymbol)
-async def forbidden_symbol_handler(req: starlette.requests.Request, exc: ForbiddenSymbol):
-    return JSONResponse(
-        content=StatusResponseModel(status="Error", message=exc.eng, ru=exc.ru).model_dump(), status_code=400
     )
 
 

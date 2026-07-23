aefb28c500bbe60f149a129dcce1751fc6e7cc22
rifcrat <felxuchenov@gmail.com>
2026-07-23 15:51:38 +0300

    fix status update logic

--- modal_backend/utils/services.py
+++ modal_backend/utils/services.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from requests import Session
 
@@ -46,22 +46,43 @@ class NoteService:
     @classmethod
     async def update_status(cls, db: Session, id: int) -> Note:
         note = Note.get(session=db.session, id=id)
-        group_ids = note.group_ids
-        for group_id in group_ids:
-            Group.get(session=db.session, id=group_id)
-        service_ids = note.service_ids
-        for service_id in service_ids:
-            Service.get(session=db.session, id=service_id)
-        end_ts = note.end_ts
-        time = datetime.utcnow()
-        if note.is_always == False and time >= end_ts:
-            raise ForbiddenAction(Note)
-        else:
-            if note.status == ModalStatus.ACTIVE:
-                updated_note = Note.update(id=id, session=db.session, status=ModalStatus.ARCHIVED)
-            else:
-                updated_note = Note.update(id=id, session=db.session, status=ModalStatus.ACTIVE)
+        if note.status == ModalStatus.ACTIVE:
+            updated_note = Note.update(id=id, session=db.session, status=ModalStatus.ARCHIVED)
             return updated_note
+        else:
+            group_ids = note.group_ids
+            new_group_ids = []
+            for group_id in group_ids:
+                group = db.session.query(Group).filter(Group.id == group_id, Group.is_deleted == False).first()
+                if group is not None:
+                    new_group_ids.append(group.id)
+            if new_group_ids == []:
+                raise ForbiddenAction(Note)
+
+            service_ids = note.service_ids
+            new_service_ids = []
+            for service_id in service_ids:
+                service = (
+                    db.session.query(Service).filter(Service.id == service_id, Service.is_deleted == False).first()
+                )
+                if service is not None:
+                    new_service_ids.append(service.id)
+            if new_service_ids == []:
+                raise ForbiddenAction(Note)
+
+            end_ts = note.end_ts
+            time = datetime.now(timezone.utc).replace(tzinfo=None)
+            if note.is_always == False and time >= end_ts:
+                raise ForbiddenAction(Note)
+            else:
+                updated_note = Note.update(
+                    id=id,
+                    session=db.session,
+                    group_ids=new_group_ids,
+                    service_ids=new_service_ids,
+                    status=ModalStatus.ACTIVE,
+                )
+                return updated_note
 
 
 class NoteTypeService:
